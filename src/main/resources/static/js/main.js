2d95b2ac76f1adb48e52ca7b2c8a9bd200a8a9ec
Ashkan Zarifian <zs.ashkan@yahoo.com>
2020-02-18 12:55:13 +0330

    some layout improvements

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -45,6 +45,104 @@ $(document).ready(function () {
             this.qs2.cache();
         }
     });
+
+    // Persian date picker
+    // TODO: it doesn't work correctly
+    // $('.persianDate').pDatepicker({
+    //     inline: false,
+    //     format: "LLL",
+    //     viewMode: "day",
+    //     initialValue: false,
+    //     initialValueType: "gregorian",
+    //     minDate: 1579971647710,
+    //     maxDate: 1580922047727,
+    //     autoClose: true,
+    //     position: [0,0],
+    //     altFormat: "lll",
+    //     altField: "#altfieldExample",
+    //     onlyTimePicker: false,
+    //     onlySelectOnDate: false,
+    //     calendarType: "persian",
+    //     inputDelay: 800,
+    //     observer: false,
+    //     calendar: {
+    //         persian: {
+    //             locale: "fa",
+    //             showHint: true,
+    //             leapYearMode: "algorithmic"
+    //         },
+    //         gregorian: {
+    //             locale: "en",
+    //             showHint: true
+    //         }
+    //     },
+    //     navigator: {
+    //         enabled: true,
+    //         scroll: {
+    //             enabled: true
+    //         },
+    //         text: {
+    //             btnNextText: "<",
+    //             btnPrevText: ">"
+    //         }
+    //     },
+    //     toolbox: {
+    //         enabled: true,
+    //         calendarSwitch: {
+    //             enabled: true,
+    //             format: "MMMM"
+    //         },
+    //         todayButton: {
+    //             enabled: true,
+    //             text: {
+    //                 fa: "امروز",
+    //                 en: "Today"
+    //             }
+    //         },
+    //         submitButton: {
+    //             enabled: true,
+    //             text: {
+    //                 fa: "تایید",
+    //                 en: "Submit"
+    //             }
+    //         },
+    //         text: {
+    //             btnToday: "امروز"
+    //         }
+    //     },
+    //     timePicker: {
+    //         enabled: false,
+    //         step: 1,
+    //         hour: {
+    //             enabled: false,
+    //             step: null
+    //         },
+    //         minute: {
+    //             enabled: false,
+    //             step: null
+    //         },
+    //         second: {
+    //             enabled: false,
+    //             step: null
+    //         },
+    //         meridian: {
+    //             enabled: false
+    //         }
+    //     },
+    //     dayPicker: {
+    //         enabled: true,
+    //         titleFormat: "YYYY MMMM"
+    //     },
+    //     monthPicker: {
+    //         enabled: true,
+    //         titleFormat: "YYYY"
+    //     },
+    //     yearPicker: {
+    //         enabled: true,
+    //         titleFormat: "YYYY"
+    //     },
+    //     responsive: true
+    // });
 });
 
 
